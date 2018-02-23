8e75446338118de482065608fa040a994ede7a6c
Guillaume Martigny <guillaume.martigny@gmail.com>
2018-02-23 23:42:57 +0100

    Build task with webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+    entry: "./pencil.js",
+    plugins: [
+        new UglifyJsPlugin(),
+    ],
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"],
+                },
+            },
+        }],
+    },
+    output: {
+        filename: "dist/text-direction.min.js",
+        library: "Pencil",
+        libraryTarget: "this",
+        libraryExport: "default",
+    },
+};
