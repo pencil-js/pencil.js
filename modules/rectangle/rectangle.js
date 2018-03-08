83147cf40f3cb122ca8b2ee5fe1e0482d5e4e45b
Guillaume Martigny <guillaume.martigny@gmail.com>
2018-03-08 22:18:56 +0100

    Add test for rectangle

--- modules/rectangle/rectangle.js
+++ modules/rectangle/rectangle.js
@@ -35,13 +35,8 @@ export default class Rectangle extends Component {
      * @return {Boolean}
      */
     isHover (position) {
-        const strokeModifier = this.options.stroke ? this.options.strokeWidth / 2 : 0;
-        const x = this.position.x - strokeModifier;
-        const y = this.position.y + strokeModifier;
-        const width = this.width + strokeModifier;
-        const height = this.height + strokeModifier;
         return super.isHover(position) &&
-            x <= position.x && position.x <= this.position.x + width &&
-            y <= position.y && position.y <= this.position.y + height;
+            this.position.x <= position.x && position.x <= this.position.x + this.width &&
+            this.position.y <= position.y && position.y <= this.position.y + this.height;
     }
 }
---
+++ modules/rectangle/rectangle.test.js
@@ -0,0 +1,29 @@
+/* global test expect jest */
+
+import Position from "@pencil.js/position";
+import Rectangle from "./rectangle";
+
+test("Rectangle creation and trace", () => {
+    const w = 123;
+    const h = 22.2;
+    const rect = new Rectangle(new Position(-20, 20), w, h);
+
+    expect(rect.width).toBe(w);
+    expect(rect.height).toBe(Math.floor(h));
+
+    const ctx = {
+        rect: jest.fn(),
+    };
+    rect.trace(ctx);
+    expect(ctx.rect.mock.calls.length).toBe(1);
+    expect(ctx.rect.mock.calls[0]).toEqual([0, 0, w, Math.floor(h)]);
+});
+
+
+test("Rectangle isHover", () => {
+    const rect = new Rectangle(new Position(22, -10), 77, 13);
+
+    expect(rect.isHover(new Position(55, -2))).toBe(true);
+    expect(rect.isHover(new Position(99, 3))).toBe(true);
+    expect(rect.isHover(new Position(10, 5))).toBe(false);
+});
