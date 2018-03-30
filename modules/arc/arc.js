9137da2dbc3e75093c075428f418a072537c7bb9
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2018-03-30 15:53:11 +0200

    Arc behave more like a line and fix angle

--- modules/arc/arc.js
+++ modules/arc/arc.js
@@ -1,6 +1,7 @@
 import Component from "@pencil.js/component";
-import Vector from "@pencil.js/vector";
+import Line from "@pencil.js/line";
 import Position from "@pencil.js/position";
+import Vector from "@pencil.js/vector";
 import { truncate, radianCircle } from "@pencil.js/math";
 
 /**
@@ -14,8 +15,8 @@ export default class Arc extends Component {
      * @param {Position} position - Center of arc
      * @param {Number} radius - Distance from center to outer edge
      * @param {Number} [startAngle=0] - Angle to start from (0 is top, 0.5 is bottom and 1 is full circle back to top)
-     * @param {Number} [endAngle=0.5] - Angle to end to
-     * @param {ComponentOptions} [options] - Drawing options
+     * @param {Number} [endAngle=0.5] - Angle to end to (from startAngle and in clockwise rotation)
+     * @param {LineOptions} [options] - Drawing options
      */
     constructor (position, radius, startAngle = 0, endAngle = 0.5, options) {
         super(position, options);
@@ -30,7 +31,10 @@ export default class Arc extends Component {
      * @return {Arc} Itself
      */
     trace (ctx) {
-        ctx.arc(0, 0, truncate(this.radius), this.startAngle * radianCircle, this.endAngle * radianCircle);
+        ctx.lineCap = this.options.cap;
+        const startAngle = (this.startAngle - 0.25) * radianCircle;
+        const endAngle = (this.endAngle - 0.25) * radianCircle;
+        ctx.arc(0, 0, truncate(this.radius), startAngle, endAngle);
         return this;
     }
 
@@ -41,9 +45,23 @@ export default class Arc extends Component {
      */
     isHover (position) {
         const radius = truncate(this.radius);
-        const top = new Position(0, radius);
+        const top = new Position(0, -radius);
         const flatPart = new Vector(top.rotate(this.startAngle), top.rotate(this.endAngle));
         return super.isHover(position) &&
             this.position.distance(position) <= radius && position.isOnSameSide(this.position, flatPart);
+    }
+
+    /**
+     * @return {LineOptions}
+     */
+    static get defaultOptions () {
+        return Line.defaultOptions;
+    }
+
+    /**
+     * @return {LineCaps}
+     */
+    static get caps () {
+        return Line.caps;
     }
 }
