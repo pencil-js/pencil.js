72f53c00360a32724267aaa8c479a6105e4cd5ac
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2018-07-11 09:44:34 +0200

    Update Arc tests

--- modules/arc/arc.test.js
+++ modules/arc/arc.test.js
@@ -9,6 +9,8 @@ describe("Arc", () => {
     });
 
     test("creation", () => {
+        expect(arc.position.x).toBe(100);
+        expect(arc.position.y).toBe(50);
         expect(arc.radius).toBe(20);
         expect(arc.startAngle).toBeCloseTo(0.1);
         expect(arc.endAngle).toBeCloseTo(0.6);
@@ -18,7 +20,7 @@ describe("Arc", () => {
         const path = new Path2D();
         arc.trace(path);
 
-        expect(path.arc.mock.calls.length).toBe(1);
+        expect(path.arc).toHaveBeenCalledTimes(1);
         const PI2 = Math.PI * 2;
         const expected = [0, 0, 20, (0.1 - 0.25) * PI2, (0.6 - 0.25) * PI2];
         path.arc.mock.calls[0].forEach((value, index) => expect(value).toBeCloseTo(expected[index]));
@@ -30,5 +32,19 @@ describe("Arc", () => {
         expect(json.radius).toBe(20);
         expect(json.startAngle).toBeCloseTo(0.1);
         expect(json.endAngle).toBeCloseTo(0.6);
+    });
+
+    describe("statics", () => {
+        test("defaultOptions", () => {
+            expect(Arc.defaultOptions.cap).toBe(Arc.caps.round);
+            expect(Arc.defaultOptions.fill).not.toBeDefined();
+            expect(Arc.defaultOptions.join).not.toBeDefined();
+        });
+
+        test("caps", () => {
+            expect(Arc.caps.butt).toBe("butt");
+            expect(Arc.caps.round).toBe("round");
+            expect(Arc.caps.square).toBe("square");
+        });
     });
 });
