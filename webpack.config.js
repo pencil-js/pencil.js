9b60af1197dc55e0f3d2176c0e20a834203d57f3
Guillaume Martigny <guillaume.martigny@gmail.com>
2018-07-29 00:47:46 +0200

    :heavy_minus_sign: Removing a dependency. Remove babel since it comes with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,5 @@
 module.exports = {
     entry: "./pencil.js",
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ["@babel/preset-env"],
-                },
-            },
-        }],
-    },
     output: {
         filename: "pencil.min.js",
         library: "Pencil",
