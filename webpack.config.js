5877a385ccb0e6f653c83484c281b93b9e786ee5
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2018-08-06 11:24:15 +0200

    :ambulance: Critical hotfix. Add Babel, fix for running on codepen

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
+    },
     entry: "./pencil.js",
     output: {
         filename: "pencil.min.js",
