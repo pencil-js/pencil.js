bdb8fde6ba5d446ddc17ae905984bbc8d283494e
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2018-09-12 17:14:33 +0200

    :page_facing_up: Adding or updating license. Add license info in built file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const { resolve } = require("path");
+const { BannerPlugin } = require("webpack");
+
+const { name, version, homepage, author, license } = require("./modules/pencil.js/package.json");
 
 const mainModule = "./modules/pencil.js/";
 
@@ -17,6 +20,10 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new BannerPlugin(`${name} v${version} ${homepage}
+${license} license - © ${author}`),
+    ],
     entry: resolve(__dirname, `${mainModule}pencil.js`),
     output: {
         path: resolve(__dirname, `${mainModule}dist`),
