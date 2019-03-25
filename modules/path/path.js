9adc8dde450921e93f1f932308c0cb603fd4316d
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2019-03-25 10:26:32 +0100

    :recycle: Refactoring code. Split Instruction class into its own file

--- modules/path/path.js
+++ modules/path/path.js
@@ -2,55 +2,7 @@ import Component from "@pencil.js/component";
 import Line from "@pencil.js/line";
 import Position from "@pencil.js/position";
 import Spline from "@pencil.js/spline";
-
-/**
- * Instruction class
- * @class
- */
-class Instruction {
-    /**
-     * @callback InstructionCallback
-     * @param {CanvasRenderingContext2D} ctx
-     * @param {Position} target
-     * @param {Position} previousPosition
-     */
-    /**
-     * Instruction constructor
-     * @param {InstructionCallback} action - Function to execute
-     * @param {PositionDefinition} target - Position to go to
-     */
-    constructor (action, target) {
-        this.action = action;
-        this.target = Position.from(target);
-    }
-
-    /**
-     * Follow the instruction
-     * @param {Path2D} path - Current drawing path
-     * @param {Position} previousPosition - Position from where the instruction start
-     * @return {Position} The position reached
-     */
-    execute (path, previousPosition) {
-        this.action.call(this, path, this.target, previousPosition);
-        return this.target;
-    }
-
-    /**
-     * Unimplemented
-     * FIXME: How to serialize a wrapper of a function ?
-     */
-    toJSON () { // eslint-disable-line class-methods-use-this
-        throw new ReferenceError("Unimplemented toJSON function in Instruction");
-    }
-
-    /**
-     * Unimplemented
-     * FIXME
-     */
-    static from () {
-        throw new ReferenceError("Unimplemented from function in Instruction");
-    }
-}
+import Instruction from "./instruction";
 
 /**
  * Path class
