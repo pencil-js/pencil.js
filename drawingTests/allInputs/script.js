99318b09d3c7acb58f78388207007fdb426e272e
Guillaume Martigny <guillaume.martigny@gmail.com>
2019-09-12 17:34:30 +0200

    :white_check_mark: Adding tests. Add drawing test for Knob

--- drawingTests/allInputs/script.js
+++ drawingTests/allInputs/script.js
@@ -5,20 +5,32 @@
 
     const scene = new P.Scene(window.allInputsScene);
 
+    const slider = new P.Slider([20, 10], {
+        value: 7,
+    });
+    const progress = new P.ProgressBar([20, 40], {
+        value: 0.7,
+    });
+    const knob = new P.Knob([40, 130], {
+        radius: 20,
+        value: 7,
+    });
+    slider.on(P.Input.events.change, () => {
+        progress.value = slider.value / 10;
+        knob.value = slider.value;
+    });
+    knob.on(P.Input.events.change, () => {
+        progress.value = knob.value / 10;
+        slider.value = knob.value;
+    });
+
     scene.add(
-        new P.Checkbox([20, 20], {
-            value: true,
-        }),
-        new P.Slider([20, 50], {
-            value: 8,
-        }),
-        new P.ProgressBar([20, 80], {
-            value: 0.8,
-        }),
-        new P.Button([20, 110], {
+        slider,
+        progress,
+        new P.Button([20, 70], {
             value: "Click",
         }),
-        new P.Select([90, 110], [
+        new P.Select([90, 70], [
             "Unicorn",
             "Doggy",
             "Cat",
@@ -26,6 +38,10 @@
         ], {
             value: 1,
         }),
+        new P.Checkbox([195, 75], {
+            value: true,
+        }),
+        knob,
     );
 
     scene.startLoop();
