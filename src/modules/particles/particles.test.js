cb01ab0c1cd5bba44e2583b316ab8346e1db2bb0
Guillaume Martigny <guillaume.martigny@gmail.com>
2021-02-05 14:27:36 +0100

    Update tests and readme

--- src/modules/particles/particles.test.js
+++ src/modules/particles/particles.test.js
@@ -4,9 +4,10 @@ import Particles from ".";
 test.beforeEach((t) => {
     t.context = new Particles([10, 20], {
         trace: () => "",
-    }, 10, i => ({
+    }, (i, additional) => ({
         position: [100, 200],
-        rotation: i,
+        i,
+        ...additional,
     }), (data, i) => {
         data.called = i;
     });
@@ -14,24 +15,36 @@ test.beforeEach((t) => {
 
 test("constructor", (t) => {
     t.true(typeof t.context.base === "object");
-    t.is(t.context.data.length, 10);
+    t.is(t.context.data.length, 0);
+});
+
+test("generate and trace", (t) => {
+    t.context.generate(20, {
+        add: 42,
+        ttl: 1,
+    });
+    t.is(t.context.data.length, 20);
     t.context.data.forEach((data, i) => {
         t.true(data.position.constructor.name === "Position");
         t.is(data.position.x, 100);
         t.is(data.position.y, 200);
-        t.is(data.rotation, i);
+        t.is(data.i, i);
+        t.is(data.add, 42);
+        t.is(data.ttl, 1);
         t.is(data.called, undefined);
     });
-});
 
-test("trace", (t) => {
-    t.context.trace({
+    const ctx = {
         addPath: () => t.pass(),
-    });
-    t.plan(20);
+    };
+    t.context.trace(ctx);
     t.context.data.forEach((data, i) => {
         t.is(data.called, i);
+        t.is(data.ttl, 0);
     });
+
+    t.context.trace(ctx);
+    t.is(t.context.data.length, 0);
 });
 
 test("isHover", (t) => {
@@ -42,7 +55,7 @@ test("toJSON", (t) => {
     const json = t.context.toJSON();
 
     t.truthy(json.base);
-    t.is(json.data.length, 10);
+    t.is(json.data.length, 0);
 });
 
 test.todo("from");
--- src/modules/particles/readme.md
+++ src/modules/particles/readme.md
@@ -10,21 +10,22 @@ Particle generator optimized to display thousands of instances.
 ```js
 import { Particles, Star, Math as M } from "pencil.js";
 
+const count = 5000;
 const position = [100, 200];
 const base = new Star();
-const count = 5000;
-const generator = () => {
+const generator = (index, any, additional, params) => {
     return {
         position: scene.getRandomPosition(),
         rotation: M.random(),
         // And whatever value you want to define for each individual particle
     };
 };
-const updater = (data) => {
+const updater = (data, index) => {
     data.position.add(data.speed);
 };
 
-const particles = new Particles(position, base, count, generator, updater);
+const particles = new Particles(position, base, generator, updater);
+particles.generate(count, any, additional, params);
 ```
 
 
