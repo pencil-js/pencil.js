17acfe6bcf043c14547ab915faf07a1331e6b822
Parsa <106576103+curedbylethe@users.noreply.github.com>
2022-11-10 14:51:57 +0330

    ✨ Add createConicGradient API
    Adds Conic Gradient #98

---
+++ src/modules/conic-gradient/conic-gradient.test.js
@@ -0,0 +1,32 @@
+import test from "ava";
+import ConicGradient from ".";
+
+test.beforeEach((t) => {
+    t.context = new ConicGradient([0, 0], 0, {
+        0: "red",
+        1: "blue",
+    });
+});
+
+test("constructor", (t) => {
+    t.is(t.context.position.constructor.name, "Position");
+    t.is(t.context.radius, 0);
+    t.is(typeof t.context.colorStops, "object");
+});
+
+test("toString", (t) => {
+    const expected = [
+        ["0", "red"],
+        ["1", "blue"],
+    ];
+    const ctx = {
+        createConicGradient: () => ({
+            itsMe: "lethe",
+            addColorStop: (...args) => t.deepEqual(args, expected[ctx.call++]),
+        }),
+        call: 0,
+    };
+    t.plan(3);
+    const gradient = t.context.toString(ctx);
+    t.is(gradient.itsMe, "lethe");
+});
